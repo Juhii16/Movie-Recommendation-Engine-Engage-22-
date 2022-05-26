4c7477a51bd944bffd6a42a4b901cb622dcda0a3
Juhii16 <88276540+Juhii16@users.noreply.github.com>
2022-05-27 01:39:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -380,10 +380,8 @@ if __name__ == '__main__':
             unsafe_allow_html=True)
 
     elif choice == "Contribute":
-	st.subheader("DocumentFiles")
-	
+	#st.subheader("DocumentFiles")
 	docx_file = st.file_uploader("Upload File",type=["txt"])
-	
 	if st.button("Process"):
 		if docx_file is not None:
 			file_details = {"Filename":docx_file.name,"FileType":docx_file.type,"FileSize":docx_file.size}
