882889fb7344051a1fa131927d423df798b32a2e
Juhii16 <88276540+Juhii16@users.noreply.github.com>
2022-05-27 01:53:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -395,11 +395,7 @@ if __name__ == '__main__':
                     '</p>', unsafe_allow_html=True)
 	
         st.subheader("Document Files")
-	uploaded_file = st.file_uploader(
-            "",
-            type=['txt'], key="2")
-        new_title = '<br></br>'
-        st.markdown(new_title, unsafe_allow_html=True)
+	st.button('ssds')
 	
 	
     elif choice == "Feedback form":
