42cca8049878e8503351ea2182e42428b1b2a349
Juhii16 <88276540+Juhii16@users.noreply.github.com>
2022-05-26 23:48:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -187,6 +187,15 @@ if __name__ == '__main__':
         if ch:
             st.write('Thank you For Agreeing')
     elif choice == "Login":
+           file_ = open("hello.gif", "rb")
+            contents = file_.read()
+            data_url = base64.b64encode(contents).decode("utf-8")
+            file_.close()
+            st.markdown(
+                f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+                unsafe_allow_html=True,
+            )
+
 
    
 
